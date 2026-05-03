4f05cd07766d4ab5f98bce71c23b9a4a93eb3a31
herk72 <harkherk86@gmail.com>
2026-05-04 00:43:24 +0300

    Create collect.php

---
+++ collect.php
@@ -0,0 +1,9 @@
+<?php
+// إعدادات تيليجرام
+define('TELEGRAM_TOKEN', 'حط_التوكن_بتاعك_هنا');
+define('TELEGRAM_CHAT_ID', 'حط_الايدي_بتاعك_هنا');
+
+// إعدادات الشركة والمطور
+define('COMPANY_URL', 'https://example.com'); // رابط الشركة الأم اللي هيتحول عليه
+define('DEV_USERNAME', 'YourUsername'); // يوزر المطور (بدون علامة @)
+?>
