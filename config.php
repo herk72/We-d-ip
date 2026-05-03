db71812250adafc17cb9eb97a719ed0faf3d4834
herk72 <harkherk86@gmail.com>
2026-05-04 00:52:30 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // إعدادات تيليجرام
-define('TELEGRAM_TOKEN', 'حط_التوكن_بتاعك_هنا');
-define('TELEGRAM_CHAT_ID', 'حط_الايدي_بتاعك_هنا');
+define('TELEGRAM_TOKEN', '8640735414:AAHidJ9SwpKu3xhvwv29SULBEj-ra_mLiSI');
+define('TELEGRAM_CHAT_ID', '8357381411');
 
 // إعدادات الشركة والمطور
-define('COMPANY_URL', 'https://example.com'); // رابط الشركة الأم اللي هيتحول عليه
-define('DEV_USERNAME', 'YourUsername'); // يوزر المطور (بدون علامة @)
+define('COMPANY_URL', 'https://genshin.hoyoverse.com/m/en/home'); // رابط الشركة الأم اللي هيتحول عليه
+define('DEV_USERNAME', 'He_xo0'); // يوزر المطور (بدون علامة @)
 ?>
