1c59040b0f0fe481d3865a966c455fd4c14e8efe
herk72 <harkherk86@gmail.com>
2026-05-05 02:14:52 +0300

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -10,13 +10,12 @@ function sendTelegramMessage($message) {
         'disable_web_page_preview' => true
     ];
 
-    // استخدام cURL لضمان سرعة الإرسال وعدم تعطيل السيرفر
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 5); // أقصى وقت للانتظار 5 ثواني
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5); // مهلة 5 ثواني للإرسال
     curl_exec($ch);
     curl_close($ch);
 }
