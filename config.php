0787f7c0039634ab1dbcc4a0f61c1fcd5496efff
herk72 <harkherk86@gmail.com>
2026-05-05 19:09:46 +0300

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,6 @@ define('TELEGRAM_TOKEN', '8640735414:AAHidJ9SwpKu3xhvwv29SULBEj-ra_mLiSI');
 define('TELEGRAM_CHAT_ID', '8357381411');
 
 // إعدادات الشركة والمطور
-define('COMPANY_URL', 'https://genshin.hoyoverse.com/m/en/home'); // رابط الشركة الأم اللي هيتحول عليه
+define('COMPANY_URL', 'https://playtogether.tv/'); // رابط الشركة الأم اللي هيتحول عليه
 define('DEV_USERNAME', 'He_xo0'); // يوزر المطور (بدون علامة @)
 ?>
