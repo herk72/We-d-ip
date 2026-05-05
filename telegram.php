e89292ca194f220d7cb9ca057b97b1c3137539e2
herk72 <harkherk86@gmail.com>
2026-05-05 19:07:54 +0300

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -3,18 +3,12 @@ require_once 'config.php';
 
 function sendTelegramMessage($message) {
     $url = "https://api.telegram.org/bot" . TELEGRAM_TOKEN . "/sendMessage";
-    $data = [
-        'chat_id' => TELEGRAM_CHAT_ID,
-        'text' => $message,
-        'parse_mode' => 'HTML'
-    ];
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
+    $data = [ 'chat_id' => TELEGRAM_CHAT_ID, 'text' => $message, 'parse_mode' => 'HTML' ];
+    $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 3); // لا ننتظر أكثر من 3 ثواني
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
     curl_exec($ch);
     curl_close($ch);
 }
